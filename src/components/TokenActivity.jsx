0a4870e0a812e39dbbd80234f603fce27a8fff6e
ahsanw3 <w3ellahi@gmail.com>
2022-12-13 19:47:00 +0500

    Details of Token

---
+++ src/components/TokenActivity.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TokenActivity() {
+  return (
+    <div className='rounded border-2 border-gray-900 px-20 py-5 mx-40'>
+       <p className='font-bold text-xl'>Token Activity</p>
+       <div>
+
+       </div>
+    </div>
+  )
+}
