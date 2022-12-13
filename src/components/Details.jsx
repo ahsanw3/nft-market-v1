916d55ea3d4629c90dde2bac489f14a5ff8812fa
ahsanw3 <w3ellahi@gmail.com>
2022-12-13 19:47:00 +0500

    Details of Token

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Description from "./Description";
+
 import DetailsCard from "./DetailsCard";
+import TokenActivity from "./TokenActivity";
 import pic from "/home/dev/Desktop/nftkey/src/images/nftpic.png";
 export default function Details() {
   let Data = [
@@ -15,20 +15,17 @@ export default function Details() {
       type: "cat",
       owner: "DF1942",
       status: "Not for Sale",
-      description:"Young alchemist and a great explorer. Fortune smiles at him, he always finds rich loot."
+      description:"BGP - https://opensea.io/collection/bubblegumpuppies A collection of 10,000 Unique randomly generated Bubblegum Kids! Merch Drops,Community Comic,Giveaways and much more!Come Join the BGK Fam"
     },
   ];
   return (
     <>
       <div className="flex flex-col ">
-        <div className="flex gap-10 justify-center py-20 flex-col lg:flex-row ">
+        <div className="flex gap-10 justify-center py-20 flex-col lg:flex-row">
           <img src={pic} alt="" className="lg:w-1/4 rounded " />
           <DetailsCard data={Data} />
         </div>
-        <div className=" flex justify-center">
-        <Description data={Data} />
-        </div>
-
+        <TokenActivity/>
       </div>
     </>
   );
