4f9913194870e866f19deb6802543fc191bd807a
ahsanw3 <w3ellahi@gmail.com>
2022-12-15 20:06:48 +0500

    Token activity added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import Details from './components/Details';
 
 function App() {
   return (
+    <div className="font-roboto min-h-screen flex flex-col justify-between">
     <div>
-     <Details/>
+      {/* <Navbar /> */}
+      <main className="flex justify-center px-5 sm:px-10 my-8">
+        <div className="max-w-[1400px] w-full">
+          <Details />
+        </div>
+      </main>
     </div>
+    {/* <Footer /> */}
+  </div>
   );
 }
 
