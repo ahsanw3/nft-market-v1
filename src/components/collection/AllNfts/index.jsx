520e26d53377e9d10e948610f0e5e8bb99146dc4
ahsanw3 <w3ellahi@gmail.com>
2023-03-29 17:10:11 +0500

    Deployment

--- src/components/collection/AllNfts/index.jsx
+++ src/components/collection/AllNfts/index.jsx
@@ -1,6 +1,6 @@
 import NftCard from '../NftCard'
 
-const AllNfts = ({ nfts }) => {
+const AllNfts = ({ nfts }) =>{
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5  grid-flow-row gap-4 md:gap-6">
       {nfts.map((nft) => {
